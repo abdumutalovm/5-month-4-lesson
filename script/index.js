84ef08d633ecfa20c4cc276e8bb14c82aaad303a
abdumutalovm <mirziyodabdumutalov83@gmail.com>
2024-02-15 19:08:28 +0500

    5-month 4-lesoon

--- script/index.js
+++ script/index.js
@@ -49,7 +49,6 @@ const emailVal = () => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
   };
-  alert("Malumotlar muvaffaqiyatli qoshildi :)");
 };
 
 let nameLocal = nameVal();
